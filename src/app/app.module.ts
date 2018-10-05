1e184ca8cbbb8bbd7403768733db121c31f69e57
grass1987 <king198785@yahoo.com.hk>
2018-10-05 14:09:32 +0800

    add header,todo-item,add-form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { AddFormComponent } from './add-form/add-form.component';
+import { TodoItemsComponent } from './todo-items/todo-items.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AddFormComponent,
+    TodoItemsComponent
   ],
   imports: [
     BrowserModule
