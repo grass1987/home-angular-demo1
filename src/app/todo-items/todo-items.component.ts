d54e90bd78a306549cc5615ad2c20e7e975f005f
grass1987 <king198785@yahoo.com.hk>
2018-10-06 23:34:03 +0800

    upload ngclass practice

--- src/app/todo-items/todo-items.component.ts
+++ src/app/todo-items/todo-items.component.ts
@@ -7,7 +7,18 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class TodoItemsComponent implements OnInit {
   @Input() items: TodoItem[];
-
+  getBlueClass() {
+    // 這裡可以用程式動態決定要回傳字串、陣列或物件
+    return 'blue';
+  }
+  
+  itemClick(item){
+    if(item.done==true)
+      item.done=false;
+    else
+      item.done=true;
+  }
+  
   constructor() { }
 
   ngOnInit() {
