3ed34976766b21ae7cbcc9b1385626fef795bff8
grass1987 <king198785@yahoo.com.hk>
2018-10-14 12:49:27 +0800

    directive practice with error(how to set parameter in spec.ts? )

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,16 @@ import { TodoListService } from './todo-list.service';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { TodoDoneCheckPipe } from './todo-done-check.pipe';
+import { BsButtonDirective } from './bs-button.directive';
+
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     AddFormComponent,
     TodoItemsComponent,
-    TodoDoneCheckPipe
+    TodoDoneCheckPipe,
+    BsButtonDirective
 
   ],
   imports: [
